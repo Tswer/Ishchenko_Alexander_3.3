4d9960b7303e4ee869d2eb58eef8894212edb007
Tswer <66019739+Tswer@users.noreply.github.com>
2020-05-27 18:57:55 +0300

    Lab3.3

---
+++ 3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Arr = { "mama", "papa", "brat", "asd" };
+            string s = "";
+            var result = Arr.Aggregate(s, (x, y) => s+=y[0]);
+            Console.WriteLine(result);
+        }
+    }
+}
